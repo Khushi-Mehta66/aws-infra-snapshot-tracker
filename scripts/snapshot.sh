03665d36962d089e700282487956c73949d2c7ea
Khushi Mehta <143320548+Khushi-Mehta66@users.noreply.github.com>
2025-06-27 23:04:43 +0530

    snapshot.sh

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -40,45 +40,45 @@ SNAPSHOT_DIR="/home/ec2-user/infra-snapshot-tracker/snapshots"
 mkdir -p "$SNAPSHOT_DIR"
 
 #EC2 instances
-aws ec2 describe-instances --region $REGION > "$SNAPSHOT_DIR/ec2-$DATE-$region.json"
+aws ec2 describe-instances --region $REGION > "$SNAPSHOT_DIR/ec2-$DATE.json"
 
 #S3 bucket
 aws s3api list-buckets > "$SNAPSHOT_DIR/s3-$DATE.json"
 
 #Security groups
-aws ec2 describe-security-groups --region $REGION > "$SNAPSHOT_DIR/security-group-$DATE-$region.json"
+aws ec2 describe-security-groups --region $REGION > "$SNAPSHOT_DIR/security-group-$DATE.json"
 
 #IAM users
 aws iam list-users > "$SNAPSHOT_DIR/iam-users-$DATE.json"
 
 #RDS
-aws rds describe-db-instances --region $REGION > "$SNAPSHOT_DIR/rds-$DATE-$region.json"
+aws rds describe-db-instances --region $REGION > "$SNAPSHOT_DIR/rds-$DATE.json"
 
 #Dynamodb
-aws dynamodb list-tables --region $REGION > "$SNAPSHOT_DIR/dynamodb-$DATE-$region.json"
+aws dynamodb list-tables --region $REGION > "$SNAPSHOT_DIR/dynamodb-$DATE.json"
 
 #lambda
-aws lambda list-functions --region $REGION > "$SNAPSHOT_DIR/lambda-$DATE-$region.json"
+aws lambda list-functions --region $REGION > "$SNAPSHOT_DIR/lambda-$DATE.json"
 
 #EBS
-aws ec2 describe-volumes --region $REGION > "$SNAPSHOT_DIR/ebs-$DATE-$region.json"
+aws ec2 describe-volumes --region $REGION > "$SNAPSHOT_DIR/ebs-$DATE.json"
 
 #CloudFront
-aws cloudwatch list-metrics --region $REGION > "$SNAPSHOT_DIR/cloudfront-$DATE-$region.json"
+aws cloudwatch list-metrics --region $REGION > "$SNAPSHOT_DIR/cloudfront-$DATE.json"
 
 #SNS
-aws sns list-topics --region $REGION > "$SNAPSHOT_DIR/sns-$DATE-$region.json"
+aws sns list-topics --region $REGION > "$SNAPSHOT_DIR/sns-$DATE.json"
 
 #Route53
-aws route53 list-hosted-zones > "$SNAPSHOT_DIR/route53-$DATE-$region.json"
+aws route53 list-hosted-zones > "$SNAPSHOT_DIR/route53-$DATE.json"
 
 #VPC
-aws ec2 describe-vpcs --region $REGION > "$SNAPSHOT_DIR/vpc-$DATE-$region.json"
+aws ec2 describe-vpcs --region $REGION > "$SNAPSHOT_DIR/vpc-$DATE.json"
 
 #ELB
-aws elb describe-load-balancers --region $REGION > "$SNAPSHOT_DIR/elb-$DATE-$region.json"
+aws elb describe-load-balancers --region $REGION > "$SNAPSHOT_DIR/elb-$DATE.json"
 
 #CloudFormation
-aws cloudformation describe-stacks --region $REGION > "$SNAPSHOT_DIR/cloudformation-$DATE-$region.json"
+aws cloudformation describe-stacks --region $REGION > "$SNAPSHOT_DIR/cloudformation-$DATE.json"
 
 echo "Snapshots of $region region saved for $DATE"
